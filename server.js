6ac0b9ddda625507419128d269210069ed901906
Jovan <jocoraos@yahoo.com>
2021-05-31 14:59:37 +0200

    k

--- server.js
+++ server.js
@@ -5,14 +5,7 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const app = express();
-app.use(cors(), function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 app.use(express.json());
 app.use("/", router);
 
